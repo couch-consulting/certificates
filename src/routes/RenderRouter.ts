f1517731553015ff82f7c1b1a6959440c5399acb
Thore <thore@kruess.xyz>
2018-06-10 03:07:02 +0200

    Broken html-pdf implementation

--- src/routes/RenderRouter.ts
+++ src/routes/RenderRouter.ts
@@ -1,14 +1,20 @@
 'use strict';
 
-import Database from '../models/Database';
 import { Router, Request, Response, NextFunction } from 'express';
 import { json } from 'body-parser';
 import * as nunjucks from 'nunjucks';
+import * as htmlPdf from 'html-pdf';
+import * as phantom from 'phantom';
+import * as fs from 'fs-extra';
+
+import Database from '../models/Database';
 import { templateData, extendedTemplateObject } from '../models/Interfaces';
 
 export class RenderRouter {
   router: Router;
   private database: Database;
+  private instance;
+  private page;
 
 
   /**
@@ -18,23 +24,39 @@ export class RenderRouter {
     this.database = new Database();
     this.router = Router();
     nunjucks.configure({ autoescape: true });
+    phantom.create().then((phantom) => {
+      this.instance = phantom;
+      this.instance.createPage().then((page) => {
+        this.page = page;
+      });
+    });
+
     this.init();
   }
 
   /**
    * POST call to render a new certificate
    */
-  public renderCertificate(req: Request, res: Response, next: NextFunction) {
+  public async renderCertificate(req: Request, res: Response, next: NextFunction) {
+    let filepath: string = process.env.STORAGE_PATH || '/tmp/';
+    filepath += req.params.taskId;
     this.database.getWorkItem(req.params.taskId).then((taskData: templateData) => {
-      console.log('Got work item');
-      console.log(taskData);
       this.database.checkTemplateId(taskData.templateId).then(() => {
         this.database.getTemplate(taskData.templateId).then((templateData: extendedTemplateObject) => {
-          console.log('Got template data');
-          console.log(templateData);
           const certificate: string = nunjucks.renderString(templateData.previewHTML, taskData);
           console.log(certificate);
-          res.sendStatus(200);
+          const testhtml: string = fs.readFileSync('./bcard.html', 'utf8');
+
+          // Unfortunately only callbacks are supported. gnah.
+          htmlPdf.create(testhtml).toFile(filepath, (err, result) => {
+            if (err) {
+              console.log(err);
+              res.statusMessage = 'An error occurred while generating the certificate';
+              res.sendStatus(500);
+            }
+            console.log(result);
+            res.sendStatus(200);
+          });
         }).catch((err) => {
           res.sendStatus(500);
         });
